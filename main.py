a6031929d351dbb21fdbb09cb080435382c5a296
Teerapong64 <83325716+Teerapong64@users.noreply.github.com>
2021-08-15 23:38:57 +0700

    dos.py

--- main.py
+++ main.py
@@ -4,3 +4,166 @@ os.system("wget https://google.com")
 os.system("clear")
 print("AHAHAHAHAHAHHA")
 os.system("python dos.py")
+import os
+
+os.system("wget https://google.com")
+os.system("clear")
+print("AHAHAHAHAHAHHA")
+os.system("python dos.py")
+import os
+
+os.system("wget https://google.com")
+os.system("clear")
+print("AHAHAHAHAHAHHA")
+os.system("python dos.py")
+import os
+
+os.system("wget https://google.com")
+os.system("clear")
+print("AHAHAHAHAHAHHA")
+os.system("python dos.py")
+import os
+
+os.system("wget https://google.com")
+os.system("clear")
+print("AHAHAHAHAHAHHA")
+os.system("python dos.py")
+import os
+
+os.system("wget https://google.com")
+os.system("clear")
+print("AHAHAHAHAHAHHA")
+os.system("python dos.py")
+import os
+
+os.system("wget https://google.com")
+os.system("clear")
+print("AHAHAHAHAHAHHA")
+os.system("python dos.py")
+import os
+
+os.system("wget https://google.com")
+os.system("clear")
+print("AHAHAHAHAHAHHA")
+os.system("python dos.py")
+import os
+
+os.system("wget https://google.com")
+os.system("clear")
+print("AHAHAHAHAHAHHA")
+os.system("python dos.py")
+import os
+
+os.system("wget https://google.com")
+os.system("clear")
+print("AHAHAHAHAHAHHA")
+os.system("python dos.py")
+import os
+
+os.system("wget https://google.com")
+os.system("clear")
+print("AHAHAHAHAHAHHA")
+os.system("python dos.py")
+import os
+
+os.system("wget https://google.com")
+os.system("clear")
+print("AHAHAHAHAHAHHA")
+os.system("python dos.py")
+import os
+
+os.system("wget https://google.com")
+os.system("clear")
+print("AHAHAHAHAHAHHA")
+os.system("python dos.py")
+
+
+import os
+
+os.system("wget https://google.com")
+os.system("clear")
+print("AHAHAHAHAHAHHA")
+os.system("python dos.py")
+import os
+
+os.system("wget https://google.com")
+os.system("clear")
+print("AHAHAHAHAHAHHA")
+os.system("python dos.py")import os
+
+os.system("wget https://google.com")
+os.system("clear")
+print("AHAHAHAHAHAHHA")
+os.system("python dos.py")
+import os
+
+os.system("wget https://google.com")
+os.system("clear")import os
+
+os.system("wget https://google.com")
+os.system("clear")
+print("AHAHAHAHAHAHHA")
+os.system("python dos.py")
+
+print("AHAHAHAHAHAHHA")
+os.system("python dos.py")
+
+
+
+
+
+import os
+
+os.system("wget https://google.com")
+os.system("clear")
+print("AHAHAHAHAHAHHA")
+os.system("python dos.py")
+import os
+
+os.system("wget https://google.com")
+os.system("clear")
+print("AHAHAHAHAHAHHA")
+os.system("python dos.py")
+
+import os
+
+os.system("wget https://google.com")
+os.system("clear")
+print("AHAHAHAHAHAHHA")
+os.system("python dos.py")
+
+import os
+
+os.system("wget https://google.com")
+os.system("clear")
+print("AHAHAHAHAHAHHA")
+os.system("python dos.py")
+
+import os
+
+os.system("wget https://google.com")
+os.system("clear")
+print("AHAHAHAHAHAHHA")
+os.system("python dos.py")
+
+
+import os
+
+os.system("wget https://google.com")
+os.system("clear")
+print("AHAHAHAHAHAHHA")
+os.system("python dos.py")
+import os
+
+os.system("wget https://google.com")
+os.system("clear")
+print("AHAHAHAHAHAHHA")
+os.system("python dos.py")
+
+import os
+
+os.system("wget https://google.com")
+os.system("clear")
+print("AHAHAHAHAHAHHA")
+os.system("python dos.py")
+
